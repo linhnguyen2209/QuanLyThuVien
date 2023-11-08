4dcd5ffb0c0e5dfcd9f103c97c56fba85711c57a
Thương <123073000+thuonghnh1@users.noreply.github.com>
2023-11-08 16:35:28 +0700

    Add files via upload
    phieutradao

---
+++ src/main/java/quanlythuvien/dao/PhieuTraDAO.java
@@ -0,0 +1,69 @@
+package quanlythuvien.dao;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import quanlythuvien.entity.PhieuTra;
+import quanlythuvien.utils.jdbcHelper;
+
+/**
+ *
+ * @author thuon
+ */
+public class PhieuTraDAO extends ThuVienDAO<PhieuTra, String>{
+    final String INSERT_SQL = "INSERT INTO PhieuTra (MaPhieuMuon, NgayTra, TrangThai, MaNguoiDung, GhiChu) VALUES (?,?,?,?,?)";
+    final String UPDATE_SQL = "UPDATE PhieuTra set MaPhieuMuon = ?, NgayTra = ?, TrangThai = ?, MaNguoiDung = ?, GhiChu = ? where MaPhieuTra = ?";
+    final String DELETE_SQL = "DELETE FROM PhieuTra where MaNguoiDung = ?";
+    final String SELECT_ALL_SQL = "SELECT * FROM PhieuTra";
+    final String SELECT_BY_ID_SQL = "SELECT * FROM PhieuTra where MaPhieuTra = ?";
+
+    @Override
+    public void insert(PhieuTra entity) {
+        jdbcHelper.update(INSERT_SQL, entity.getMaPhieuTra(), entity.getMaPhieuMuon(), entity.getNgayTra(), entity.isTrangThai(), entity.getMaNguoiDung(), entity.getGhiChu());
+    }
+
+    @Override
+    public void update(PhieuTra entity) {
+        jdbcHelper.update(UPDATE_SQL,entity.getMaPhieuMuon(), entity.getNgayTra(), entity.isTrangThai(), entity.getMaNguoiDung(), entity.getGhiChu(), entity.getMaNguoiDung());
+    }
+
+    @Override
+    public void delete(String id) {
+        jdbcHelper.update(DELETE_SQL, id);
+    }
+
+    @Override
+    public List<PhieuTra> selectAll() {
+        return selectBySql(SELECT_ALL_SQL);
+    }
+
+    @Override
+    public PhieuTra selectById(String id) {
+        List<PhieuTra> list = selectBySql(SELECT_BY_ID_SQL, id);
+        if (list.isEmpty()) {
+            return null;
+        }
+        return list.get(0);
+    }
+
+    @Override
+    public List<PhieuTra> selectBySql(String sql, Object... args) {
+        List<PhieuTra> list = new ArrayList<>();
+        try {
+            ResultSet rs = jdbcHelper.query(sql, args);
+            while (rs.next()) {
+                PhieuTra entity = new PhieuTra();
+                entity.setMaPhieuTra(rs.getInt("MaPhieuTra"));
+                entity.setMaPhieuMuon(rs.getInt("MaPhieuMuon"));
+                entity.setNgayTra(rs.getDate("NgayTra"));
+                entity.setTrangThai(rs.getBoolean("TrangThai"));
+                entity.setMaNguoiDung(rs.getString("MaNguoiDung"));
+                entity.setGhiChu(rs.getString("GhiChu"));
+                list.add(entity);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return list;
+    }
+}
