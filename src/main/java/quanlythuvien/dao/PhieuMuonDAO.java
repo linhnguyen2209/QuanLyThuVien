4d0c39b9fee06521fa45789663dbb896ed3badbf
thuong <thuong@BaiQi>
2023-11-10 20:10:12 +0700

    UPDATE QL PHIEU MUON lần n+1

--- src/main/java/quanlythuvien/dao/PhieuMuonDAO.java
+++ src/main/java/quanlythuvien/dao/PhieuMuonDAO.java
@@ -1,20 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package quanlythuvien.dao;
 
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
-import quanlythuvien.entity.NguoiDung;
 import quanlythuvien.entity.PhieuMuon;
 import quanlythuvien.utils.jdbcHelper;
 
 public class PhieuMuonDAO extends ThuVienDAO<PhieuMuon, Integer> {
 
     final String INSERT_SQL = "INSERT INTO PhieuMuon (NgayMuon, NgayHenTra, TongSoLuongSachMuon, MaNguoiDung, GhiChu) VALUES (?,?,?,?,?)";
-    final String UPDATE_SQL = "update PhieuMuon set  NgayMuon = ? ,NgayHenTra =?, TongSoLuongSachMuon, MaNguoiDung = ?, GhiChu = ? where MaPhieuMuon = ?";
+    final String UPDATE_SQL = "update PhieuMuon set  NgayMuon = ? ,NgayHenTra = ?, TongSoLuongSachMuon = ?, MaNguoiDung = ?, GhiChu = ? where MaPhieuMuon = ?";
     final String DELETE_SQL = "DELETE from PhieuMuon where MaPhieuMuon = ?";
     final String SELECT_ALL_SQL = "select * from PhieuMuon";
     final String SELECT_BY_ID_SQL = "select * from PhieuMuon where MaPhieuMuon = ?";
